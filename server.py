4d115e24e091805ab1ad84d14a1eff12a4fbbb42
Roscoe <elysiaroscoe@gmail.com>
2021-10-29 14:38:48 -0700

    adding y variable

--- server.py
+++ server.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", num=8, numy=8)
 
 @app.route('/<int:num>')
 def version2(num):
-    return render_template("index.html", num = num)
+    return render_template("index.html", num = num, numy=num)
+
+@app.route('/<int:num>/<int:numy>')
+def version3(num,numy):
+    return render_template("index.html", num = num, numy=numy)
 
 
 if __name__=="__main__":
